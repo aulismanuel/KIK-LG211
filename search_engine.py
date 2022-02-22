ee222ce17b5da474512b5305e0f8d16bd8b599b3
Aulis Manuel Korva <aulismanuel@Aulis-MacBook-Pro.local>
2022-02-22 05:01:19 +0200

    #7 Made a module out of 100 wikipedia articles and tried it worked as supposed

--- search_engine.py
+++ search_engine.py
@@ -1,24 +1,58 @@
-#   DDDD      A     TTTTT     A
-#   D   D    A A      T      A A
-#   D   D   AAAAA     T     AAAAA
-#   DDDD   A     A    T    A     A
+#   DATA
 
 documents = [
     "This is an example that is different from the one online",
     "A better example indeed",
     "I just want to play around a bit",
     "I must say this example is a bit better than the online one",
-    "And I add this one example more"
+    "And I add this one example more",
+    "There is really nothing great here"
 ]
 
-#    QQQ    U   U   EEEEE   RRRR    Y   Y       PPPP    RRRR     OOO    M   M   PPPP    TTTTT
-#   Q   Q   U   U   E       R   R    Y Y        P   P   R   R   O   O   MM MM   P   P     T
-#   Q Q Q   U   U   EEE     RRRR      Y         PPPP    RRRR    O   O   M M M   PPPP      T
-#   Q  QQ   U   U   E       R   R     Y         P       R   R   O   O   M   M   P         T
-#    QQQ Q   UUU    EEEEE   R   R     Y         P       R   R    OOO    M   M   P         T
+import documents_library
+wiki100 = documents_library.wiki100
 
-queryInput = input("Searching for: ")
-print("Searching for \"" + queryInput + "\"...") # So this is just a placeholder to be replaced with correct functionality
+#   QUERY PROMPT
+
+#queryInput = input("Searching for: ")
+#print("Searching for \"" + queryInput + "\"...") # So this is just a placeholder to be replaced with correct functionality
 
 #   MAKING A MATRIX
 
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(lowercase=True, binary=True)
+sparse_matrix = cv.fit_transform(wiki100)     # HERE THE DATA SOURCE IS CHOSEN
+dense_matrix = sparse_matrix.todense()
+
+td_matrix = dense_matrix.T
+
+#   SEARCHING
+
+t2i = cv.vocabulary_
+
+            # Operators and/AND, or/OR, not/NOT become &, |, 1 -
+            # Parentheses are left untouched
+            # Everything else interpreted as a term and fed through td_matrix[t2i["..."]]
+
+d = {"and": "&", "AND": "&",
+     "or": "|", "OR": "|",
+     "not": "1 -", "NOT": "1 -",
+     "(": "(", ")": ")"}          # operator replacements
+
+def rewrite_token(t):
+    return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # Can you figure out what happens here?
+
+def rewrite_query(query): # rewrite every token in the query
+    return " ".join(rewrite_token(t) for t in query.split())
+
+def test_query(query):
+    print("Query: '" + query + "'")
+    print("Rewritten:", rewrite_query(query))
+    print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
+    print()
+
+test_query("example AND NOT nothing")
+test_query("NOT example OR great")
+test_query("( NOT example OR great ) AND nothing") # AND, OR, NOT can be written either in ALLCAPS
+test_query("( not example or great ) and nothing") # ... or all small letters
+test_query("not example and not nothing")
